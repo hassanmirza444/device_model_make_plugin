09b76f6bfc38d7bd0ca7d35de76dedecd887fe7e
Hassan Mirza <hassanmirza444@gmail.com>
2023-07-25 12:59:32 +0500

    improved IOS.

--- ios/Classes/DeviceModelMakePlugin.swift
+++ ios/Classes/DeviceModelMakePlugin.swift
@@ -3,15 +3,20 @@ import UIKit
 
 public class DeviceModelMakePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "device_model_make_plugin", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name:  "hassanmirza444.github.com/deviceModelMakePlugin", binaryMessenger: registrar.messenger())
     let instance = DeviceModelMakePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "getDeviceModelMake":
+                let deviceversion = UIDevice.current.systemVersion
+                let modelName = UIDevice.current.model
+                let osName = UIDevice.current.systemName
+            let modelMake = "Model : \(modelName)\nOS : \(osName)\nVersion : \(deviceversion)"
+
+      result(modelMake)
     default:
       result(FlutterMethodNotImplemented)
     }
